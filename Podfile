c5d54d490534c9ab3ff4f7047555218590718bc3
Jamone Kelly <xg.monie089@gmail.com>
2015-12-07 14:52:22 -0500

    - Add ChameleonFramework, and FontAwesomeKit pod - Set the global app them to flat sky blue - Ask user for location access - Show where our user currently is located

---
+++ Podfile
@@ -0,0 +1,4 @@
+use_frameworks!
+
+pod 'ChameleonFramework/Swift'
+pod 'FontAwesomeKit', '~> 2.1.0'
--- School For Me/MapVC.swift
+++ School For Me/MapVC.swift
@@ -7,17 +7,49 @@
 //
 
 import UIKit
+import FontAwesomeKit
+import MapKit
+import CoreLocation
 
-class MapVC: UIViewController {
-
+class MapVC: UIViewController, MKMapViewDelegate {
+    @IBOutlet var mapView: MKMapView!
+    @IBOutlet var searchBar: UISearchBar!
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // VC Title
+        self.title = "School For Me"
+        
+        // Search Icon from FAK
+        let searchIcon = FAKFontAwesome.searchIconWithSize(25).imageWithSize(CGSize(width: 30, height: 30))
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: searchIcon, style: .Plain, target: self, action: nil)
+        
+        // Delegates
+        self.mapView.delegate = self
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.requestWhenInUseAuthorization()
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+            
+            self.mapView.setUserTrackingMode(MKUserTrackingMode.FollowWithHeading, animated: true);
+            
+        } else {
+            let alert = UIAlertController(title: "Error", message: "Please enable location services in your settings application.", preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+            return
+        }
+        
+        mapView.showsUserLocation = true // Show current location of user
+        mapView.mapType = MKMapType.Standard // Standard look of the map
+        mapView.zoomEnabled = true // Allow the user to zoom on the map via finger gestures
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
