a42f0aaffb47290f3be75c5d8f8640b664416ab5
Jamone Kelly <xg.monie089@gmail.com>
2015-12-07 13:40:26 -0500

    Initial Commit

---
+++ School For Me/MapVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  School For Me
+//
+//  Created by Jamone Alexander Kelly on 12/6/15.
+//  Copyright © 2015 Jamone Kelly. All rights reserved.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ School For MeTests/School_For_MeTests.swift
@@ -0,0 +1,36 @@
+//
+//  School_For_MeTests.swift
+//  School For MeTests
+//
+//  Created by Jamone Alexander Kelly on 12/6/15.
+//  Copyright © 2015 Jamone Kelly. All rights reserved.
+//
+
+import XCTest
+@testable import School_For_Me
+
+class School_For_MeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
